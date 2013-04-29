18591d08b734642f915f0aa64deaeee3338d1b8f
jiunjiun <quietmes@gmail.com>
2013-04-30 03:29:17 +0800

    Add lang basic, frontend, login, setting

---
+++ web/application/language/zh-TW/basic_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_name']         = 'IPPS';
+$lang['login']            = '登入';
+$lang['account']          = '帳戶';
+$lang['add']              = '新增';
+$lang['edit']             = '修改';
+$lang['save']             = '儲存';
+$lang['del']              = '刪除';
+$lang['cancel']           = '取消';
+$lang['clear']            = '清除';
+$lang['search']           = '搜尋';
+$lang['submit']           = '確認';
+$lang['class']            = '類別';
+$lang['pselect']          = '請選擇';
+$lang['name']             = '姓名';
+$lang['identity']         = '身分證';
+$lang['gender']           = '性別';
+$lang['birthday']         = '生日';
+$lang['gender_1']         = '男';
+$lang['gender_2']         = '女';
+$lang['phone']            = '電話';
+$lang['email']            = 'Email';
+$lang['address']          = '地址';
+$lang['error']            = 'Error!';
+$lang['mes_DoYouWantDel'] = '確定刪除?';
+
---
+++ web/application/language/zh-TW/login_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['login_error_account'] 				= '輸入您的使用者名稱。';
+$lang['login_error_password'] 				= '使用者名稱或密碼有誤。';
