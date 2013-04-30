71dd86b89e94f53e6b7d77108c9e56131a98ab02
jiunjiun <quietmes@gmail.com>
2013-04-30 10:16:02 +0800

    Add backend page index, _header, _footer

---
+++ web/application/views/backend/_header.php
@@ -0,0 +1,7 @@
+<div class="masthead">
+  <ul class="nav nav-pills pull-right">
+    <li class="active"><a href="<?php echo site_url('/'); ?>"><?php echo $lang->line('front-end'); ?></a></li>
+    <li><a href="<?php echo site_url('backend/safety').'/'; ?>"><?php echo $lang->line('safety'); ?></a></li>
+  </ul>
+  <h3 class="muted"><?php echo $lang->line('web_name');?></h3>
+</div>
---
+++ web/application/views/backend/index.php
@@ -0,0 +1,12 @@
+<div class="row-fluid marketing">
+  <div class="span6">
+    <h4><a href="<?php echo site_url('/backend/safety').'/'; ?>"><?php echo $lang->line('safety');?></a></h4>
+    <p><?php echo $lang->line('safety_des'); ?></p>
+  </div>
+  <!--
+  <div class="span6">
+    <h4>Subheading</h4>
+    <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+  </div>
+  -->
+</div>
