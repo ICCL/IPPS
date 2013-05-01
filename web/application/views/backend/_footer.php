76d08270ea8f65dbdd66db40783de47f58590f28
jiunjiun <quietmes@gmail.com>
2013-05-01 11:18:47 +0800

    Update views/backend/_footer, backend/style 增加頁尾實驗室的logo跟標示以及style

--- web/application/views/backend/_footer.php
+++ web/application/views/backend/_footer.php
@@ -1,3 +1,6 @@
 <div class="footer">
-  <p>&copy; Company 2013</p>
+    <div class="span2"><img src="<?php echo site_url('statics/img/logo.jpg'); ?>"/></div>
+    <div class="span7">
+        <p>&copy; Company 2013 國立海洋科技大學 電訊工程系(所) 智慧型通訊控制實驗室</p>
+    </div>
 </div>
