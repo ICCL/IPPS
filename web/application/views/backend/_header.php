2f88fd7b1ee1613d3d84dfe798c6a67dcaba45c7
jiunjiun <quietmes@gmail.com>
2013-05-01 11:17:19 +0800

    Update views/backend/_header 增加目前所在只的指示, 提醒使用者

--- web/application/views/backend/_header.php
+++ web/application/views/backend/_header.php
@@ -1,7 +1,7 @@
 <div class="masthead">
   <ul class="nav nav-pills pull-right">
-    <li class="active"><a href="<?php echo site_url('/'); ?>"><?php echo $lang->line('front-end'); ?></a></li>
-    <li><a href="<?php echo site_url('backend/safety').'/'; ?>"><?php echo $lang->line('safety'); ?></a></li>
+    <li><a href="<?php echo site_url('/'); ?>"><?php echo $lang->line('front-end'); ?></a></li>
+    <li <?php echo preg_match('/safety/', uri_string()) ? 'class="active"' : ''; ?>><a href="<?php echo site_url('backend/safety').'/'; ?>"><?php echo $lang->line('safety'); ?></a></li>
   </ul>
-  <h3 class="muted"><?php echo $lang->line('web_name');?></h3>
+  <h3 class="muted"><a href="<?php echo site_url('backend/').'/'; ?>"><?php echo $lang->line('web_name');?></a></h3>
 </div>
