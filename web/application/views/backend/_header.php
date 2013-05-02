997b6297747e6dada4d46de3d96ca928392b63c8
jiunjiun <quietmes@gmail.com>
2013-05-02 15:14:58 +0800

    Update views/backend/_header.php Edit title name

--- web/application/views/backend/_header.php
+++ web/application/views/backend/_header.php
@@ -3,5 +3,5 @@
     <li><a href="<?php echo site_url('/'); ?>"><?php echo $lang->line('front-end'); ?></a></li>
     <li <?php echo preg_match('/safety/', uri_string()) ? 'class="active"' : ''; ?>><a href="<?php echo site_url('backend/safety').'/'; ?>"><?php echo $lang->line('safety'); ?></a></li>
   </ul>
-  <h3 class="muted"><a href="<?php echo site_url('backend/').'/'; ?>"><?php echo $lang->line('web_name');?></a></h3>
+  <h3 class="muted"><a href="<?php echo site_url('backend/').'/'; ?>">Backend | <?php echo $lang->line('web_name');?></a></h3>
 </div>
