a6d564af6971a621282104202859c7a3e26615f5
jiunjiun <quietmes@gmail.com>
2013-05-02 15:14:03 +0800

    Update view/backend.php Edit title and update jQuery version

--- web/application/views/backend.php
+++ web/application/views/backend.php
@@ -2,14 +2,14 @@
 <html lang="zh-TW">
     <head>
         <meta http-equiv="content-type" content="text/html; charset=utf-8">
-        <title>IPPS | Backend</title>
+        <title>Backend | IPPS</title>
         <!-- css styles -->
         <link href="/ipps/statics/css/normalize.css" rel="stylesheet" />
         <link href="/ipps/statics/lib/bootstrap/docs/assets/css/bootstrap.css" rel="stylesheet" />
         <link rel="stylesheet" href="/ipps/statics/css/backend.css" type="text/css" media="screen" charset="utf-8">
 
         <!-- js -->
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
         <script src="/ipps/statics/lib/bootstrap/docs/assets/js/bootstrap.min.js"></script>
         <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
         <!--[if lt IE 9]>
