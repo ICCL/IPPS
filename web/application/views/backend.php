29d6c2bed4a1f574b31ff8784d000db59feffa3a
jiunjiun <quietmes@gmail.com>
2013-05-05 07:09:48 +0800

    Update views/backend url use code igniter function

--- web/application/views/backend.php
+++ web/application/views/backend.php
@@ -4,13 +4,13 @@
         <meta http-equiv="content-type" content="text/html; charset=utf-8">
         <title>Backend | IPPS</title>
         <!-- css styles -->
-        <link href="/ipps/statics/css/normalize.css" rel="stylesheet" />
-        <link href="/ipps/statics/lib/bootstrap/docs/assets/css/bootstrap.css" rel="stylesheet" />
-        <link rel="stylesheet" href="/ipps/statics/css/backend.css" type="text/css" media="screen" charset="utf-8">
+        <link href="<?php echo site_url('statics/css/normalize.css');?>" rel="stylesheet" />
+        <link href="<?php echo site_url('statics/lib/bootstrap/docs/assets/css/bootstrap.css');?>" rel="stylesheet" />
+        <link rel="stylesheet" href="<?php echo site_url('statics/css/backend.css');?>" type="text/css" media="screen" charset="utf-8">
 
         <!-- js -->
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
-        <script src="/ipps/statics/lib/bootstrap/docs/assets/js/bootstrap.min.js"></script>
+        <script src="<?php echo site_url('statics/lib/bootstrap/docs/assets/js/bootstrap.min.js');?>"></script>
         <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
         <!--[if lt IE 9]>
         <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
