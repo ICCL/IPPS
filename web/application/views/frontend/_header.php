f24a0f321997fc9e2c8b1aa084e3befa04a8e7f8
jiunjiun <quietmes@gmail.com>
2013-05-05 07:20:28 +0800

    Add 前端頁面

---
+++ web/application/views/frontend/_header.php
@@ -0,0 +1,15 @@
+<div class="masthead">
+  <h3 class="muted"><a href="<?php echo site_url('/'); ?>"><?php echo $lang->line('web_name'); ?></a></h3>
+  <div class="navbar">
+    <div class="navbar-inner">
+      <div class="container">
+        <ul class="nav">
+          <li><a href="#Humidity" data-toggle="tab">Humidity</a></li>
+          <li><a href="#Light" data-toggle="tab">Light</a></li>
+          <li><a href="#Soil" data-toggle="tab">Soil</a></li>
+          <li><a href="#Temperature" data-toggle="tab">Temperature</a></li>
+        </ul>
+      </div>
+    </div>
+  </div><!-- /.navbar -->
+</div>
---
+++ web/application/views/frontend/index.php
@@ -0,0 +1,21 @@
+<div class="tab-content">
+    <div class="tab-pane fade text-center active in" id="home">
+        <h1>Chart !!</h1>
+    </div>
+    <div class="tab-pane fade text-center" id="Humidity">
+        <h1>Humidity</h1>
+        <div id="chartHumidity" class="Chart"></div>
+    </div>
+    <div class="tab-pane fade text-center" id="Light">
+        <h1>Light</h1>
+        <div id="chartLight" class="Chart"></div>
+    </div>
+    <div class="tab-pane fade text-center" id="Soil">
+        <h1>Soil</h1>
+        <div id="chartSoil" class="Chart"></div>
+    </div>
+    <div class="tab-pane fade text-center" id="Temperature">
+        <h1>Temprature</h1>
+        <div id="chartTemperature" class="Chart"></div>
+    </div>
+</div>
