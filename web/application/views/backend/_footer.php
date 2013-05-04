5309ac626e147d601146f896c8d3db03e7c829fb
jiunjiun <quietmes@gmail.com>
2013-05-05 07:11:45 +0800

    Update view/backend/_footer Update footer content

--- web/application/views/backend/_footer.php
+++ web/application/views/backend/_footer.php
@@ -2,7 +2,7 @@
   <a href="http://iccl.nkmu.edu.tw">
     <div class="span2"><img src="<?php echo site_url('statics/img/logo.jpg'); ?>"/></div>
     <div class="span7">
-        <p>&copy; Company 2013 國立海洋科技大學 電訊工程系(所) 智慧型通訊控制實驗室</p>
+        <p>&copy; 2013 國立海洋科技大學 電訊工程系(所) 智慧型通訊控制實驗室</p>
     </div>
   </a>
 </div>
