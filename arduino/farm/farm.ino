15f522f238b9d0fa3440ff3728fa6472f99ac357
jiunjiun <quietmes@gmail.com>
2013-05-15 18:13:59 +0800

    Update arduino/ farm 針對Ethernet增加接收的功能

--- arduino/farm/farm.ino
+++ arduino/farm/farm.ino
@@ -4,99 +4,68 @@
 
   #define dataPin  6
   #define clockPin 7
+  
   byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED}; // 乙太網路
-  IPAddress server(192, 168, 0, 104);                // IP
+  IPAddress ip(192, 168, 0, 30);                     // local IP
+  IPAddress remote(192, 168, 0, 130);                // remote IP
+  EthernetServer Listen(80);                         // server
+  EthernetClient client;                             // client
+  
   SHT1x sht1x(dataPin, clockPin);                    // 傳感器
-  EthernetClient client;                             // 建立客戶端可以連接到指定的網絡的IP地址
+  
+  boolean EthernetTf = true;
+  String Request;                                    // stores the HTTP request
   float Temperature, Humidity;                       // 溫度、濕度
   int Soil, Light;                                   // 土壤, 光
-  boolean EthernetTf = true;
-
+  
   int time = 0;
 
   void setup() {
     Serial.begin(9600);
+    
     pinMode(A0, INPUT);             // 設定腳位位置的行為
     pinMode(9, OUTPUT);             // 同上
     pinMode(2, OUTPUT);             // 同上
     pinMode(3, OUTPUT);             // 同上
-
-
-    Serial.println("begin Ethernet..");
-    if(!verifyEthernet()) {
-      EthernetTf = false;
-      Serial.println("Failed to configure Ethernet using DHCP"); // 乙太網路連線失敗
-    }
+    
+    Ethernet.begin(mac, ip);
+    Listen.begin();
+    Serial.print("server is at ");
+    Serial.println(Ethernet.localIP());
   }
-
-  boolean verifyEthernet() {
-    if (Ethernet.begin(mac) == 0)
-      return false;
-    else
-      return true; 
-    }
-
+  
   void loop() {
-    if(EthernetTf) {
-      Temperature = sht1x.readTemperatureC(); // 溫度傳感器
-      Humidity    = sht1x.readHumidity();     // 濕度傳感器
-      int loo     = analogRead(A1);
-      Soil        = (1000-(loo-23))/10;       // 土壤
-      Light       = analogRead(A0);           // 光
-
-      /** 光的值小於299，就亮燈 **/
-//		    if(Light<299) { 
-//		    	  digitalWrite(9, HIGH);
-//		    } else {
-//			  digitalWrite(9, LOW);
-//		    }
-
-                    /** 溫度大於26度和濕度大於69，開風散 **/
-//		    if(Temperature>26|Humidity>69) { 
-//			  digitalWrite(2, HIGH);
-//		    } else {
-//			  digitalWrite(2, LOW);
-//		    }
-
-                    /** 土壤濕度小於19，開起灑水 **/
-//		    if(Soil<19) { 
-//			  digitalWrite(3, HIGH);
-//			  delay(5000);
-//			  digitalWrite(3, LOW);
-//			  delay(3000);
-//		    } else {
-//			  digitalWrite(3, LOW);
-//		    }
-
-                    /** 設定資料上傳間隔時間 time==這裡以秒為單位 **/
-      if(time==5) { 
-        Deliver();
-      }
-      time++;
-      Show();
-    } else {
-      if(!verifyEthernet()) {
-        EthernetTf = false;
-        Serial.println("Failed to configure Ethernet using DHCP"); // 乙太網路連線失敗
-      } else {
-        EthernetTf = true;
-      }
-    }
-    delay(1000);
+    EthernetSend();
+    EthernetListen();
   }
-
+  
+  void EthernetSend() {
+    Temperature = sht1x.readTemperatureC(); // 溫度傳感器
+    Humidity    = sht1x.readHumidity();     // 濕度傳感器
+    int loo     = analogRead(A1);
+    Soil        = (1000-(loo-23))/10;       // 土壤
+    Light       = analogRead(A0);           // 光
+    
+    if(time == 5) { 
+      Deliver();
+      Show();
+      time = 0;
+    }
+    time++;
+  }
+  
   void Deliver() {
     Serial.println("connecting...");
-    if (client.connect(server, 80)) {
+    if (client.connect(remote, 80)) {
       Serial.println("connected");
       client.print("GET /ipps/api/index/");
-      client.print(Temperature);
-      client.print("/");
       client.print(Humidity);
+      client.print("/");
+      client.print(Light);
       client.print("/");
       client.print(Soil);
       client.print("/");
-      client.print(Light);
+      client.print(Temperature);
       client.print("/");
       client.println(" HTTP/1.0");
       client.println();
@@ -107,7 +76,6 @@
     } else {
       Serial.println("connection failed");
     }
-    time = 0;
   }
 
   void Show() {
@@ -123,4 +91,53 @@
     Serial.println("%");
     Serial.println("=========================================");
   }
-
+  
+  void EthernetListen() {
+    // Listen for incoming clients
+    EthernetClient client = Listen.available();
+    if (client) {
+      Serial.println("new client");
+      // an http request ends with a blank line
+      boolean currentLineIsBlank = true;
+      while (client.connected()) {
+        if (client.available()) {
+          char c = client.read();
+          Request += c;
+          if (c == '\n' && currentLineIsBlank) {
+            Control(Request);
+            Serial.println(Request);
+            Request = "";
+            break;
+          }
+          if (c == '\n') {
+            // you're starting a new line
+            currentLineIsBlank = true;
+          } else if (c != '\r') {
+            // you've gotten a character on the current line
+            currentLineIsBlank = false;
+          }
+        }
+      }
+      client.stop();
+      Serial.println("client disonnected");
+    }
+  }
+  
+  void Control(String Request) {
+    if(Request.indexOf("/Light=on") > -1) {
+      digitalWrite(9, HIGH);
+    } else if(Request.indexOf("/Light=off") > -1) {
+      digitalWrite(9, LOW);
+    } else if(Request.indexOf("/Fan=on") > -1) {
+      digitalWrite(2, HIGH);
+    } else if(Request.indexOf("/Fan=off") > -1) {
+      digitalWrite(2, LOW);
+    } else if(Request.indexOf("/Pump=on") > -1) {
+      digitalWrite(3, HIGH);
+      delay(5000);
+      digitalWrite(3, LOW);
+      delay(3000);
+    } else if(Request.indexOf("/Pump=off") > -1) {
+      digitalWrite(3, LOW);
+    }
+  }
