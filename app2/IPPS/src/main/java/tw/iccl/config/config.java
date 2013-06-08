de073f19080e5631d7c7057f972f739d9a1b80d0
jiunjiun <quietmes@gmail.com>
2013-06-08 14:00:18 +0800

    backup - just demo

---
+++ app2/IPPS/src/main/java/tw/iccl/config/config.java
@@ -0,0 +1,19 @@
+package tw.iccl.config;
+
+/**
+ * Created by Macintosh on 13/6/2.
+ */
+public class config {
+
+    /* GCM */
+    public static final String SENDER_ID = "527534329368";
+
+    public static final String DISPLAY_MESSAGE_ACTION = "com.google.android.gcm.demo.app.DISPLAY_MESSAGE";
+
+    public static final String EXTRA_MESSAGE = "KEY";
+
+    public static String UserRegistrarId = "";
+
+    /* Background Service Status */
+    public static boolean haveBackgroundService = false;
+}
