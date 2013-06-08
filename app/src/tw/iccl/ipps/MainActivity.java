4c0ce6b0f1b4dc5e3d17c7754a95f292e832d98a
jiunjiun <quietmes@gmail.com>
2013-06-08 23:49:30 +0800

    backup - just demo

--- app/src/tw/iccl/ipps/MainActivity.java
+++ app/src/tw/iccl/ipps/MainActivity.java
@@ -1,23 +1,58 @@
 package tw.iccl.ipps;
 
+import static tw.iccl.service.BackgroundService.haveBackgroundService;
+import tw.iccl.service.BackgroundService;
+import tw.iccl.view.Sensor;
+import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 
-public class MainActivity extends Activity {
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+
+public class MainActivity extends SherlockActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+    protected void onStart() {
+        super.onStart();
+        onReceiver();
+    }
+	
+	@Override
+	protected void onResume() {
+		super.onRestart();
+		
+		Sensor mSensor = new Sensor(this);
+		
+	}
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void onDestroy() {
+        super.onDestroy();
+        unReceiver();
     }
 
+    private void onReceiver() {
+        if(haveBackgroundService){
+            Intent intent = new Intent(this, BackgroundService.class);
+            startService(intent);
+        }
+    }
+
+    private void unReceiver() {
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+    	MenuInflater mMenuInflater = getSupportMenuInflater();
+    	mMenuInflater.inflate(R.menu.main, menu);
         return true;
     }
-    
 }
