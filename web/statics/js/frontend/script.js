e774416079e0a76803eb568fb04b98f5f8b6ee46
jiunjiun <quietmes@gmail.com>
2013-06-13 01:21:41 +0800

    Update script.js Add nodejs use this refresh

--- web/statics/js/frontend/script.js
+++ web/statics/js/frontend/script.js
@@ -2,18 +2,43 @@ google.load("visualization", "1", {packages:["corechart"]});
 google.setOnLoadCallback(drawChart);
 function drawChart() {
   $(function() {
-    var time = 5000;
-    var Run, Title;
+    var time = 5000, Run, RunStatus = false, Title;
+
+    var socket = io.connect('http://192.168.10.103:8808');
+    //.on()為socket的接收端，client端預設的key值是connect
+    socket.on('connect', function () {
+        socket.on('update', function() {
+            console.log('update');
+            if(RunStatus) {
+                updateChart();
+            }
+        });
+    });
 
     $('ul li a').on('click', function() {
+        RunStatus = true;
         destroy();
         Title = getTitle($(this).attr('href'));
+
+        //Original_fun();
+        updateChart();
+    });
+
+    function Original_fun () {
         Run = setInterval(draw, 200);
         setTimeout(function() {
             destroy();
             Run = setInterval(draw, time);
         }, 300);
-    });
+    }
+
+    function updateChart() {
+        Run = setInterval(draw, 200);
+        setTimeout(function() {
+            destroy();
+            clearTimeout();
+        }, 300);
+    }
 
     function getTitle(href) {
         return href.substr(1, href.lenght);
